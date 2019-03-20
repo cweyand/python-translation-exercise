85487530595e2d64a00857a7dda5eee67b3437f1
Courtney Weyand <courtney.weyand@auburn.edu>
2019-03-20 12:55:03 -0500

    Added code for get longest peptide. 3 failures.

--- translate.py
+++ translate.py
@@ -108,9 +108,21 @@ def get_longest_peptide(rna_sequence, genetic_code):
     the `genetic_code`.
 
     If no amino acids can be translated from `rna_sequence` nor its reverse and
-    complement, an empty list is returned.
+    complement, an empty string is returned.
     """
-#incorrect coding. too many failures. rewrite.
+#working on code
+    pep = get_all_translations(rna_sequence = rna_sequence, genetic_code = genetic_code)
+    reverse_comp_seq = reverse_and_complement(rna_sequence)
+    reverse_comp_pep = get_all_translations(rna_sequence = reverse_comp_seq, genetic_code = genetic_code)
+    pep += reverse_comp_pep
+    if not pep:
+        return "" 
+    if len(pep) < 2:
+        return pep[0]
+    pass 
+
+
+
 
 if __name__ == '__main__':
     genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
